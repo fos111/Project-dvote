e457ab7dda2d84d167149b944b4ad1cadfe00412
Sofiene hal <sofienehalila@gmail.com>
2023-12-04 18:41:47 +0100

    test

---
+++ client/src/component/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => (
+  <>
+    <div className="footer-block"></div>
+    <div className="footer">
+      <div className="footer-container">
+        
+        <p>
+          Made with <i className="fas fa-heartbeat" /> for RSI3 - 2023.
+          
+          
+          
+        </p>
+      </div>
+    </div>
+  </>
+);
+
+export default Footer;
